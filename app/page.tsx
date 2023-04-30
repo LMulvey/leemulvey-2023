b4cfdd429b129741c4bacbe9e6e1793aff51de8b
Lee Mulvey <lmulvey@me.com>
2023-04-30 09:57:11 -0600

    feat: basics setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <article className="prose prose-slate lg:prose-xl mt-8">
-      <h2>Hey, I am Lee 👋🏼</h2>
+      <div className="flex flex-row items-center gap-8">
+        <Image
+          className="rounded-full mt-0 mb-0 lg:mt-0 lg:mb-0"
+          src="/lee.webp"
+          width={84}
+          height={84}
+          alt="Lee Mulvey"
+        />
+        <h2 className="mt-0 mb-0 lg:mt-0 lg:mb-0">Hey, I am Lee 👋🏼</h2>
+      </div>
+      <p>
+        I&apos;m a weird dad, avid gamer, cyclist, and full-stack engineer from{" "}
+        <strong>Calgary, AB</strong>.
+      </p>
       <p>
         With over 8 years of experience in both frontend and backend
         development, I have the expertise you need to bring your project to
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { Fredoka_One } from "next/font/google";
 import { cx } from "class-variance-authority";
 import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
+import Image from "next/image";
 
 const fredoka = Fredoka_One({
   subsets: ["latin"],
@@ -23,36 +27,72 @@ const descriptors = [
   "Sweat of the Seas 🏴‍☠️",
 ];
 
-const randomDescriptor = (() => {
-  const descriptor = descriptors[Math.floor(Math.random() * descriptors.length)];
+const randomDescriptor = () => {
+  const descriptor =
+    descriptors[Math.floor(Math.random() * descriptors.length)];
   return descriptor;
-})();
+};
+
+let interval: ReturnType<typeof setInterval> | undefined;
 
 export const Logo = ({ className }: { className?: string }) => {
+  const [descriptor, setDescriptor] = useState(descriptors[0]);
+
+  const onMouseOver = useCallback(() => {
+    if (interval) {
+      clearInterval(interval);
+    }
+
+    interval = setInterval(() => {
+      setDescriptor(randomDescriptor());
+    }, 1_00);
+  }, []);
+
+  const onMouseOut = useCallback(() => {
+    if (interval) {
+      clearInterval(interval);
+    }
+  }, []);
+
+  useEffect(() => {
+    // no-op
+    return () => {
+      // clear interval on unmount - helps with hot reloading
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, []);
+
   return (
-    <Link className="focus:bg-slate-300/50 rounded-md hover:scale-105 transition-transform" href="/">
-    <header className={`${className} select-none`}>
-      <h1
-        className={cx([
-          fredoka.className,
-          "text-4xl",
-          "lg:text-5xl",
-          "font-extrabold",
-          "text-transparent",
-          "bg-clip-text",
-          "bg-gradient-to-r",
-          "from-orange",
-          "to-salsa",
-          "pb-2",
-          "m-0",
-        ])}
-      >
-        Lee Mulvey
-      </h1>
-      <p className="font-mono text-xl font-extrabold p-0 -mt-2 text-slate-700/80">
-        {randomDescriptor}
-      </p>
-    </header>
+    <Link
+      className="focus:bg-slate-300/50 rounded-md hover:scale-105 transition-transform"
+      href="/"
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+    >
+      <header className={`${className} select-none`}>
+        <h1
+          className={cx([
+            fredoka.className,
+            "text-4xl",
+            "lg:text-5xl",
+            "font-extrabold",
+            "text-transparent",
+            "bg-clip-text",
+            "bg-gradient-to-r",
+            "from-orange",
+            "to-salsa",
+            "pb-2",
+            "m-0",
+          ])}
+        >
+          Lee Mulvey
+        </h1>
+        <p className="font-mono text-;g font-extrabold p-0 -mt-2 text-slate-700/80">
+          {descriptor}
+        </p>
+      </header>
     </Link>
   );
 };
